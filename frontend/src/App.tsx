e72a8291ee2c01bc2171020796029a04cc3e3598
Sidharth <sidharthinfernal@gmail.com>
2025-02-06 21:12:32 +0530

    added new landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import LandingPage from "./components/LandingPage"
-
+import {Routes,Route} from "react-router";
 function App() {
 
   return (
     <div className="overflow-x-hidden">
-    <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+      </Routes>
     </div>
   )
 }
